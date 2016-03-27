26568a9cf660c9210d634ec899414496714a71d2
Nick Rutten <hello@nickrttn.me>
2016-03-27 12:01:30 +0200

    add helmet and compression

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var compression = require('compression');
+var helmet = require('helmet');
 
 var routes = require('./routes/index');
 var polls = require('./routes/polls');
@@ -13,6 +15,10 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// helmet and compression
+app.use(helmet());
+app.use(compression());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
