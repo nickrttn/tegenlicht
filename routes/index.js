cef142497e3e12b0a8564fe7f05b0dc22c16cab5
Nick Rutten <hello@nickrttn.me>
2016-04-03 10:58:31 +0200

    clean up

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,21 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'VPRO Tegenlicht' });
+  res.render('index', {
+    title: 'VPRO Tegenlicht'
+  });
 });
 
 router.get('/voor-uitzending', (req, res, next) => {
-  res.render('voor-uitzending', { title: 'VPRO Tegenlicht' });
+  res.render('voor-uitzending', {
+    title: 'VPRO Tegenlicht'
+  });
 });
 
 router.get('/gesprek', (req, res, next) => {
-  res.render('gesprek', { title: 'VPRO Tegenlicht' });
+  res.render('gesprek', {
+    title: 'VPRO Tegenlicht'
+  });
 });
 
 module.exports = router;
